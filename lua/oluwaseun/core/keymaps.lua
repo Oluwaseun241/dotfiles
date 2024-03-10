4fe20e7c9e8a7ba8ac22f0e749cef7391fa1967b
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2024-03-10 15:50:41 +0100

    add codesnap

--- lua/oluwaseun/core/keymaps.lua
+++ lua/oluwaseun/core/keymaps.lua
@@ -49,6 +49,9 @@ keymap.set("n", "<leader>/", ":ToggleTerm<CR>") -- toggle terminal
 keymap.set("n", "<leader>gat", "<cmd>GoAddTag<cr>")
 keymap.set("n", "<leader>grt", "<cmd>GoRmTag<cr>")
 
+-- Code snap
+keymap.set("n", "<leader>cs", "<cmd>CodeSnap<cr>")
+
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
